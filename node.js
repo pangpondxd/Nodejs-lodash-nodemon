fd0f6dcd3b80f0b9609665832f20191c4737cf94
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-01-04 08:58:37 +0700

    Add files via upload
    install nodemon
    sudo npm install nodemon -g 
    install lodash
    sudo npm install lodash --save

---
+++ node.js
@@ -0,0 +1,5 @@
+console.log('starting node.js');
+const notes = require('./notes.js');
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
---
+++ notes.js
@@ -0,0 +1,9 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = function () {
+  console.log('addNote');
+  return 'สวัสดี';
+}
+module.exports.add =  function(a,b) {
+  return a + b;
+}
